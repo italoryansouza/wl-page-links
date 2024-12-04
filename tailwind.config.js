470c470aa31b9fc9c245118aad3f05d5a6696220
Ítalo Ryan <italoryansouza2019@gmail.com>
2024-12-04 02:10:31 -0300

    Teste Funcional

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'yellow01': '#644210',
+        'yellow02': '#F9D169',
+      },
+    },
+  },
+  plugins: [],
+}
+
